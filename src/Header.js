1de60af58af82fd25e22aa618d2a7d33dae60ea2
NatiWorlds <natiworlds@gmail.com>
2023-08-11 14:52:14 +0200

    corrigiendo contacto

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header(){
         <h1>Natiworlds.com</h1>
         <div id="content-header">
             <p id='parrafo-header'>Este es mi portfolio, se aceptan contrataciones mediante correo. Se facilita el contacto mediante el botón de contacto</p>
-            <h2 id='contacto-header'><a href='natiworlds@gmail.com'>Contacto</a></h2>
+            <h2 id='contacto-header'><a href='mailto:natiworlds@gmail.com'>Contacto</a></h2>
         </div>
         
         <Nav id="nav-superior"/>

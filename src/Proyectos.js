d8c033ffa652c30fb71142f4bec902c99ef1f3fb
NatiWorlds <natiworlds@gmail.com>
2023-08-11 13:03:48 +0200

    arreglando producción, y añadida la web resposive

--- src/Proyectos.js
+++ src/Proyectos.js
@@ -13,13 +13,13 @@ function Proyectos(){
                         <iframe src="https://naatii.github.io/natiworlds.com"></iframe>
                     </Accordion.Body>
                 </Accordion.Item> */}
-                <Accordion.Item eventKey='1' className='item'>
+                <Accordion.Item eventKey='0' className='item'>
                     <Accordion.Header><p>Juego del botón</p></Accordion.Header>
                     <Accordion.Body>
                         <iframe src='https://naatii.github.io/JuegoBoton/'></iframe>
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey='2' className='item'>
+                <Accordion.Item eventKey='1' className='item'>
                     <Accordion.Header><p>Catering de pepes</p></Accordion.Header>
                     <Accordion.Body>
                         <iframe src='https://cateringdepepes.com'></iframe>

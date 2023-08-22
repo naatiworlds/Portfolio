aaf41846f9236b1e84dd5359ed0937e1a7596189
NatiWorlds <natiworlds@gmail.com>
2023-08-22 12:03:24 +0200

    Prueba de netligy con github

--- src/Proyectos.js
+++ src/Proyectos.js
@@ -7,19 +7,19 @@ function Proyectos(){
         <>
             <h2>Mis Proyectos</h2>
             <Accordion className='mt-5 p-3'>
-                {/* <Accordion.Item eventKey='0' className='item'>
+                <Accordion.Item eventKey='0' className='item'>
                     <Accordion.Header><p>Tienda de ropa</p></Accordion.Header>
                     <Accordion.Body>
                         <iframe src="https://naatii.github.io/natiworlds.com"></iframe>
                     </Accordion.Body>
-                </Accordion.Item> */}
-                <Accordion.Item eventKey='0' className='item'>
+                </Accordion.Item>
+                <Accordion.Item eventKey='1' className='item'>
                     <Accordion.Header><p>Juego del botón</p></Accordion.Header>
                     <Accordion.Body>
                         <iframe src='https://naatii.github.io/JuegoBoton/' title='juegoBoton'></iframe>
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey='1' className='item'>
+                <Accordion.Item eventKey='2' className='item'>
                     <Accordion.Header><p>Catering de pepes</p></Accordion.Header>
                     <Accordion.Body>
                         <iframe src='https://cateringdepepes.com' title='catering'></iframe>
